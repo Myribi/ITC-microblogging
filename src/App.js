61751eb7daac43ddd15deb9aca38f6e490647df6
Myribi <105745901+Myribi@users.noreply.github.com>
2022-07-26 17:18:06 +0300

    M2 finished

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import CreateTweet from "./components/CreateTweet";
+import { useState, useEffect } from "react";
+import TweetList from "./components/TweetList";
+import axios from "axios";
+import Spinner from "react-bootstrap/Spinner";
+
+function App() {
+  const [tweets, setTweetsList] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [errorMessage, seterrorMessage] = useState("")
+
+  const addTweets = (PostTweet) => {
+    setTweetsList((prev) => [PostTweet, ...prev]);
+    setLoader(false);
+  };
+
+  useEffect(() => {
+    
+          (async () => {
+      const { data } = await axios.get(
+        "https://micro-blogging-dot-full-stack-course-services.ew.r.appspot.com/tweet"
+      );
+      setTweetsList(data.tweets);
+    })();
+  
+  }, []);
+
+  const errorFunc = (error) => {
+    seterrorMessage(error)
+  }
+
+  return (
+    <div className="App">
+       
+      <CreateTweet
+        tweets={tweets}
+        addTweets={addTweets}
+        setLoader={setLoader}
+        errorFunc= {errorFunc}
+        seterrorMessage={seterrorMessage}
+        
+      />
+      
+      <Spinner
+        id="loader"
+        animation="grow"
+        variant="info"
+        className={loader ? "my-2" : "d-none"}
+      />
+      {errorMessage !== "" ?<div className="text-danger mb-2 serverError">Oh no, there's seems to be a problem! Try again in a few minutes.</div> : ""}
+      <TweetList tweets={tweets} setTweetsList={setTweetsList} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CreateTweet.jsx
@@ -0,0 +1,82 @@
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
+import axios from "axios";
+
+
+export default function CreateTweet(props) {
+  const [tweet, setTweet] = useState("");
+  const [disabled, setDisabled] = useState(false);
+
+
+  const changeTweet = (e) => {
+    setTweet(e.target.value);
+    e.target.value.length > 141 ? setDisabled(true) : setDisabled(false);
+  };
+
+  const addHandler = async (e) => {
+    e.preventDefault();
+    props.seterrorMessage( "")
+    try {
+      if (tweet !== "") {
+        props.setLoader(true);
+        const PostTweet = {
+          content: tweet,
+          userName: "Myriam",
+          date: new Date().toISOString(),
+        };
+       const res = await axios.post(
+          "https://micro-blogging-dot-full-stack-course-services.ew.r.appspot.com/tweet",
+          PostTweet
+
+        );
+        console.log(res)
+        setTweet("");
+        
+        props.addTweets(res.data);
+      }
+    } catch (error) {
+     props.errorFunc(error.message)
+     props.setLoader(false)
+    }
+  };
+
+  return (
+    <>
+      <form
+        className="textarea-container border border-white mb-4"
+        onSubmit={addHandler}
+      >
+        <textarea
+          maxLength={142}
+          type="text"
+          placeholder="What's on your mind..."
+          style={{ height: "100px" }}
+          className=""
+          value={tweet}
+          onChange={changeTweet}
+        />
+
+        <div className="textarea-bottom d-flex align-items-center flex-row-reverse">
+          <Button
+            variant="primary"
+            disabled={tweet.length > 141 ? true : false}
+            type="submit"
+            className="btn ms-auto"
+          >
+            Tweet
+          </Button>
+           
+          <div
+            className="error"
+            style={{ visibility: disabled ? "visible" : "hidden" }}
+          >
+            {tweet.length > 141 ? "The tweets can't contain more than 140 chars." : ""}
+            
+
+          </div>
+
+        </div>
+      </form>
+    </>
+  );
+}
